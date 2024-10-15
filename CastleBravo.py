3d1a94925653d4d21eee47e271cb5edff0fe61e7
Winfrey <lucas.alt1614@gmail.com>
2024-10-15 08:08:48 -0400

    Added more ELIF and ELSE Statement to VRS Function - Stable

--- CastleBravo.py
+++ CastleBravo.py
@@ -16,13 +16,37 @@ weatherAlert = weather()
 def vehicleResponseSytem():
     if weatherAlert == "snowy":
         print("\nThe National Weather Service has updated our alarm by 30 minutes because"
-              " of the forecasts", weatherAlert, "weather conditions.")
+              " of the forecast of", weatherAlert, "weather conditions.")
         sleep(1)
-        print("\nVRS system has been engaged only allowing you to drive 55 mph")
+        print("\nVRS system has been engaged only allowing you to drive 55mph")
+
     elif weatherAlert == "blizzard":
         print("\nThe National Weather Service has updated our alarm by 45 minutes because"
-              " of the forecasts", weatherAlert, "weather conditions.")
+              " of the forecast of", weatherAlert, "weather conditions.")
         sleep(1)
-        print("\nVRS system has been engaged only allowing you to drive 45 mph.")
+        print("\nVRS system has been engaged only allowing you to drive 45mph.")
 
+    elif weatherAlert == "raining":
+        print("\nThe National Weather Service has updated our alarm by 15 minutes because"
+              " of the forecast of", weatherAlert, "weather conditions.")
+        sleep(1)
+        print("\nVRS system has been engaged only allowing you to drive 65mph.")
+
+    elif weatherAlert == "windy":
+        print("\nThe National Weather Service has updated our alarm by 5 minutes because"
+              " of the forecast of", weatherAlert, "weather conditions.")
+        sleep(1)
+        print("\nVRS system has been engaged only allowing you to drive 70mph.")
+
+    elif weatherAlert == "icy":
+        print("\nThe National Weather Service has updated our alarm by 50 minutes because"
+              " of the forecast of", weatherAlert, "weather conditions.")
+        sleep(1)
+        print("\nVRS system has been engaged only allowing you to drive 30mph.")
+
+    else:
+        print("\nThe National Weather Service is calling for", weatherAlert, "skies, drive carefully to your destination!")
+        sleep(1)
+        print("\nVRS system has been disengaged.")
+        
 vehicleResponseSytem()
